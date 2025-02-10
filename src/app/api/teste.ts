aa34ba233606f2fe0f94d708964b28b4f736351d
Wargas Teixeira <teixeira.wargas@gmail.com>
2025-02-10 11:02:51 -0300

    correçao

--- src/app/api/teste.ts
+++ src/app/api/teste.ts
@@ -6,7 +6,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    const session = await session();
+    const session = await auth();
 
     res.status(200).json(session?.user)
 }
