bacb4f8146a5d2a745787595b7b37d04bc662c4f
wargas <teixeira.wargas@gmail.com>
2025-02-10 08:09:07 -0300

    feat:page-login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,9 @@
 
 import { auth } from '@/auth'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import Form from 'next/form'
 import { redirect } from 'next/navigation'
 import { doLogin } from './actions'
@@ -8,16 +12,35 @@ export default async function Login() {
 
     const session = await auth()
 
-    if(session?.user) {
+    if (session?.user) {
         redirect('/dashboard')
     }
 
-    return <div>
-        <Form action={doLogin}>
+    return <div className='h-screen bg-gray-50 flex items-center w-full'>
+        <div className="w-full max-w-md mx-auto">
+            <Form action={doLogin}>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Login</CardTitle>
+                        <CardDescription>Informe suas credenciais</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div>
+                            <Label>Email</Label>
+                            <Input type='email' />
+                        </div>
 
-            <h1>Login</h1>
-
-            <button type='submit'>Entrar</button>
-        </Form>
+                        <div>
+                            <Label>Senha</Label>
+                            <Input type='password' />
+                        </div>
+                    </CardContent>
+                    <CardFooter className='flex flex-col gap-4'>
+                        <Button type='submit' className='w-full'>Entrar</Button>
+                        <Button variant={'outline'} type='submit' className='w-full'>Cadastre-se</Button>
+                    </CardFooter>
+                </Card>
+            </Form>
+        </div>
     </div>
 }
