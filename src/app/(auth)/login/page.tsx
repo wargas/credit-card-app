15fd4e13c9091704ac1dcbc04e64936e87676589
wargas <teixeira.wargas@gmail.com>
2025-02-10 15:35:07 +0000

    new-login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
-      </div>
+      </div> 
     </div>
   )
 }
