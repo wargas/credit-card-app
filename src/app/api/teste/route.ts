2276561b169e9a32b141b2cf3237554dbf235b89
Wargas Teixeira <teixeira.wargas@gmail.com>
2025-02-10 11:18:28 -0300

    teste

--- src/app/api/teste/route.ts
+++ src/app/api/teste/route.ts
@@ -4,5 +4,5 @@ import { auth } from "@/auth";
 export async function GET() {
     const session = await auth();
 
-    return Response.status(200).json(session?.user)
+    return Response.json(session?.user)
 }
